33aeedbdd4bc2ec38a351d9348bb9e118b3ebdf1
xiong35 <2242787668@qq.com>
2020-07-15 15:50:08 +0800

    css & img l;oader

--- X/Tasks/webpack/src/index.js
+++ X/Tasks/webpack/src/index.js
@@ -1,10 +1,20 @@
 import _ from "lodash";
+import "./style.css";
+import cat from "./a.jpg";
+
+console.log(cat);
 
 function component() {
   var element = document.createElement("div");
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hell", "webpack"], " ");
+  element.classList.add("hello");
+
+  var myImg = new Image();
+  myImg.src = cat;
+
+  element.appendChild(myImg);
 
   return element;
 }

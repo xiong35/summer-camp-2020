8b30ed232eae7731d767d03d28d250561aca460e
xiong35 <2242787668@qq.com>
2020-07-21 22:56:40 +0800

    half done index page request

--- X/Project/hustYJ/pages/market/market.js
+++ X/Project/hustYJ/pages/market/market.js
@@ -5,7 +5,7 @@ const infoConfig = {
   type: 2,
   category: [],
   areas: [],
-  label: "hot",
+  label: "new",
   page: 0,
   per_page: 14,
 };
@@ -14,10 +14,12 @@ Page({
   data: {
     curPage: 0,
     nameList: ["最新", "最热", "求购"],
+    items: [],
+    now: new Date() * 1,
+    itemsPage: 0,
   },
 
   handleTap(e) {
-    console.log(e);
     this.setData({
       curPage: e.target.dataset.index,
     });
@@ -27,8 +29,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
+    this.getMore(true);
+  },
+
+  async getMore(init) {
+    if (init) {
+      this.setData({
+        items: [],
+        itemsPage: 0,
+      });
+    }
     let config = {
       ...infoConfig,
+      page: this.data.itemsPage,
     };
     const res = await request(
       "market.index",
@@ -37,9 +50,12 @@ Page({
       true
     );
 
-    console.log(res);
+    this.setData({
+      items: this.data.items.concat(res.data.data),
+      itemsPage: this.data.itemsPage + 1,
+    });
+    console.log(res.data.data);
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
@@ -48,10 +64,14 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {},
+  onPullDownRefresh: function () {
+    this.getMore(true);
+  },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {},
+  onReachBottom: function () {
+    this.getMore(false);
+  },
 });

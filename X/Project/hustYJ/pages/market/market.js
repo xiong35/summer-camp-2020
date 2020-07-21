95dd8b721016c17f21237a11217110c0c9a3ef1b
xiong35 <2242787668@qq.com>
2020-07-21 20:34:01 +0800

    finish search bar

--- X/Project/hustYJ/pages/market/market.js
+++ X/Project/hustYJ/pages/market/market.js
@@ -1,66 +1,57 @@
 // pages/market/market.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    curPage: 0,
+    nameList: ["最新", "最热", "求购"],
+  },
 
+  handleTap(e) {
+    console.log(e);
+    this.setData({
+      curPage: e.target.dataset.index,
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});

712644392bac343dd7d654d81e3014877a9c7c50
xiong35 <2242787668@qq.com>
2020-07-23 15:01:36 +0800

    finish my collect

---
+++ X/Project/hustYJ/components/my-item/my-item.js
@@ -0,0 +1,31 @@
+Component({
+  properties: {
+    src_: {
+      type: String,
+      value: "../../static/img/default.jpg",
+    },
+    title: {
+      type: String,
+      value: "",
+    },
+    subtitle: {
+      type: String,
+      value: "",
+    },
+    price: {
+      type: String,
+      value: "",
+    },
+    _id: {
+      type: String,
+      value: "",
+    },
+  },
+  data: {},
+  methods: {},
+  created: function () {},
+  attached: function () {},
+  ready: function () {},
+  moved: function () {},
+  detached: function () {},
+});
--- X/Project/hustYJ/pages/myCollect/myCollect.js
+++ X/Project/hustYJ/pages/myCollect/myCollect.js
@@ -1,66 +1,46 @@
-// pages/myCollect/myCollect.js
-Page({
+import { request } from "../../network/request";
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    items: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  async onLoad(options) {
+    const res = await request(
+      "user.collection",
+      { type: 1 },
+      "GET",
+      true
+    );
+    this.setData({
+      items: res.data.data,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  async cancleCollect(e) {
+    let { _id, ind } = e.target.dataset;
 
+    const res = await request(
+      "market.collect",
+      { _id },
+      "DELETE",
+      true
+    );
+
+    if (!res.data || !res.data.success) {
+      return;
+    }
+
+    let { items } = this.data;
+    items.splice(ind, 1);
+    this.setData({
+      items,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
